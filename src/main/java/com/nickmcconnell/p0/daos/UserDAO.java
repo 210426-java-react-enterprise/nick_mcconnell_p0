856326dca5f7c0571cf20be7c70e47731a0ce4f7
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-05-04 16:05:18 -0400

    added saveUserToFile method to userDao, and built appUser constructer

--- src/main/java/com/nickmcconnell/p0/daos/UserDAO.java
+++ src/main/java/com/nickmcconnell/p0/daos/UserDAO.java
@@ -1,5 +1,27 @@
 package com.nickmcconnell.p0.daos;
 
+import com.nickmcconnell.p0.models.AppUser;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class UserDAO {
+
+    String userDataFile;
+
+    public UserDAO() {
+
+        userDataFile = "src/main/resources/users.txt";
+
+    }
+
+    public void saveUserToFile(AppUser newUser) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter((userDataFile, true))) {
+            writer.write(newUser.toFileString() + "\n");
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 
 }
--- src/main/java/com/nickmcconnell/p0/models/AppUser.java
+++ src/main/java/com/nickmcconnell/p0/models/AppUser.java
@@ -2,4 +2,74 @@ package com.nickmcconnell.p0.models;
 
 public class AppUser {
 
+    private String username;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public AppUser(String username, String password, String email, String firstName, String lastName, int age){
+        System.out.println("AppUser constructor invoked!");
+
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String toFileString(){
+        return String.format("%s;%s;%s;%s;%s;%d;", username, password, email, firstName, lastName, age);
+    }
+
 }
